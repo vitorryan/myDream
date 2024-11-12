8219ea7605adcbed742a6b303311d1507117038c
Vitor Ryan da Rosa <vitorryanrosaaa@hotmail.com>
2024-11-11 22:46:47 -0300

    Preparando o ambiente, instalando react, criando pastas iniciais, gitignore...

---
+++ loja/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div >
+      <header className="App-header">
+        <h1>Logo, Menu e Opções</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
